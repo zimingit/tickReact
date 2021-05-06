317c2a7521b7f7df1c0de9cca4d4af62267e07db
Alexey <pyksik@gmail.com>
2021-05-06 19:19:58 +0300

    FEAT ListItem handle delete

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -3,11 +3,15 @@ import ListItem from '../ListItem';
 
 import './List.scss';
 
-const List = ({data = []}) => {
+const List = ({data = [], handleDelFolder}) => {
   return (
     <ul className="sidebar__list">
       { data.map(({label, color, selected}) => (
-      <ListItem label={label} color={color} selected={selected} key={label} />
+      <ListItem label={label}
+                color={color}
+                selected={selected}
+                key={label}
+                handleDelFolder={handleDelFolder} />
       )) }
     </ul>
   );
