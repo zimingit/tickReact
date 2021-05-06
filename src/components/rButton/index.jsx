325b03b89b429db2d1ef42c6cfe51d6afa0d9cf6
Alexey <pyksik@gmail.com>
2021-05-06 19:18:12 +0300

    FEAT rButton handle click

--- src/components/rButton/index.jsx
+++ src/components/rButton/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Icon from '../Icon';
+import setClass from '../../plugins/ClassNames.js'
 
 import './rButton.scss';
 
-const rButton = ({children, icon, style, isActive}) => {
+const rButton = ({children, icon, style, isActive, className, onClick = () => {}}) => {
+  const classes = setClass(['rbutton', {isActive}, className ])
   return (
-    <button type="button" className={`rbutton ${isActive ? 'active': ''}`} style={style}>
+    <button type="button" className={classes} style={style} onClick={onClick}>
       { icon && <Icon icon={icon}/> }
       { children && <span>{children}</span> }
     </button>
