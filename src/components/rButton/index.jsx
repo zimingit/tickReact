92a342d415de32ef59fc915dccbae3445aa7db09
Alexey <pyksik@gmail.com>
2021-05-06 16:07:50 +0300

    FEAT rButton with Icon component

--- src/components/rButton/index.jsx
+++ src/components/rButton/index.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
+import Icon from '../Icon';
 
 import './rButton.scss';
 
 const rButton = ({children, icon, style, isActive}) => {
   return (
     <button type="button" className={`rbutton ${isActive ? 'active': ''}`} style={style}>
-      {icon &&
-        <i><img src={icon} alt="icon"></img></i>
-      }
-      <span>{children}</span>
+      { icon && <Icon icon={icon}/> }
+      { children && <span>{children}</span> }
     </button>
   );
 }
