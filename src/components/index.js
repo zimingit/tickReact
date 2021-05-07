436d0c0351caeed80c1990de23b59742fd49c7e6
Alexey <pyksik@gmail.com>
2021-05-07 03:11:47 +0300

    FEAT import components

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as AddFolder } from './AddFolder';
+export { default as Badge } from './Badge';
+export { default as Folder } from './Folder';
+export { default as Icon } from './Icon';
+export { default as List } from './List';
+export { default as ListBadges } from './ListBadges';
+export { default as Button } from './rButton';
+export { default as Task } from './Task';
