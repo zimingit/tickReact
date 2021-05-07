9b9f28da51c18fd1d4eaa861f5ccd59b78fbffeb
Alexey <pyksik@gmail.com>
2021-05-08 00:08:29 +0300

    FEAT Add imports & icon asset

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as AddFolder } from './AddFolder';
+export { default as AddTask } from './AddTask';
 export { default as Badge } from './Badge';
 export { default as Folder } from './Folder';
 export { default as Icon } from './Icon';
@@ -6,3 +7,4 @@ export { default as List } from './List';
 export { default as ListBadges } from './ListBadges';
 export { default as Button } from './rButton';
 export { default as Task } from './Task';
+export { default as TaskContainer } from './TaskContainer';
