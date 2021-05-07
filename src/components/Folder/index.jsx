f28918a12a0e2da3d802ad011ea2027243380cba
Alexey <pyksik@gmail.com>
2021-05-07 03:10:19 +0300

    RENAME ItemList component

---
+++ src/components/Folder/index.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import Badge from '../Badge'
+import Button from '../rButton';
+import setClass from '../../plugins/ClassNames.js'
+
+import './Folder.scss';
+import deleteIcon from '../../assets/icons/delete.svg';
+
+const ListItem = ({label, color, selected, handleDelFolder, onClick}) => {
+  // const testMultiClasses = setClass([
+  //   ['arrayFirstClass', 'arraySecondClass'],
+  //   [],
+  //   {},
+  //   'singleStringClass',
+  //   {
+  //     'objectClass': true,
+  //     'objectSecondClass': false,
+  //     'objectThirdClass': selected || true
+  //   }
+  // ])
+  const [didMount, setDidMount] = useState(false)
+  useEffect(() => {
+    setDidMount(true)
+    return () => setDidMount(false)
+  }, [])
+
+  const [isAccepted, setAccepted] = useState(false)
+  const classes = setClass(['folder', {selected}])
+  const getIcon = isAccepted ? null : deleteIcon
+  const getDeleteLabel = isAccepted ? 'ok?' : null
+
+  const handleClick = (e) => {
+    e.stopPropagation()
+    if (isAccepted) return handleDelFolder(label)
+    setAccepted(true)
+    setTimeout(() => {
+      if (!didMount) return
+      setAccepted(false)
+    }, 2000)
+  }
+
+  return (
+    <li className={classes} onClick={onClick}>
+      <Badge color={color}/>
+      <span className="label">{label}</span>
+      <div className="folder_delete">
+        <Button icon={getIcon} style={{color}} onClick={handleClick}>{getDeleteLabel}</Button>
+      </div>  
+    </li>
+  );
+}
+
+export default ListItem;
