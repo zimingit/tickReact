063b1580f1645cfbadb7c0fe4617413f94ba7e7f
Alexey <pyksik@gmail.com>
2021-05-08 19:19:49 +0300

    ADD Routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-
+import { BrowserRouter as Router, Route, Switch, Redirect  } from 'react-router-dom';
 import './index.scss';
 
+const App = lazy(() => import('./routes/App'));
+const Detailed = lazy(() => import('./routes/Detailed'));
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router basename="/tickReact">
+    <Suspense fallback={<h1>Загрузка...</h1>}>
+      <Switch>
+        <Route exact path="/" component={App}/>
+        <Route path="/:folder/:id" component={Detailed}/>
+        <Redirect to="/"/>
+      </Switch>
+    </Suspense>
+  </Router>,
   document.getElementById('root')
 );
