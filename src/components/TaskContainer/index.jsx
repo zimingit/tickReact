27e78914babaeee81bbf4c8306534a62e36f2ba2
Alexey <pyksik@gmail.com>
2021-05-08 16:41:59 +0300

    FEAT add Filter to TaskContainer

--- src/components/TaskContainer/index.jsx
+++ src/components/TaskContainer/index.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import Filter from '../Filter';
 
 import './TaskContainer.scss';
 
-const TaskContainer = ({ children, data }) => {
+const TaskContainer = ({ children, data, setFilter }) => {
   const styles = {
     color: data.color,
     borderColor: `${data.color}50`
   }
+
   return (
     <div className="task-container">
-      <h1 style={styles} className="task-container__label">{data.label}</h1>
+      <h1 style={styles} className="task-container__label">
+        {data.label}
+        <Filter {...{data, setFilter}} />
+      </h1>
       <div className="task-container__list">
         {children}
       </div>
